a7f60b824ddf23866e47e08e61baf63475bc32ad
CodingWithPriyanshu <codewithpriyanshu12@gmail.com>
2024-12-31 13:20:27 +0530

    intial file

---
+++ public/assets/assets.ts
@@ -0,0 +1,28 @@
+import banner1 from "@/public/assets/banner1.jpg";
+import banner2 from "@/public/assets/banner2.jpg";
+import banner3 from "@/public/assets/banner3.jpg";
+import banner4 from "@/public/assets/banner4.jpg";
+import astraunaut from "@/public/assets/astronaut.png";
+import gradient from "@/public/assets/gradinet.avif";
+import javaLogo from "@/public/assets/java.png";
+import pythonLogo from "@/public/assets/python.png";
+import notesLogo from "@/public/assets/notes.png";
+import openEvent from "@/public/assets/openevents.png";
+import user from "@/public/assets/user.jpg";
+import userRegister from "@/public/assets/userregister.png";
+
+const assets = {
+  banner1,
+  banner2,
+  banner3,
+  banner4,
+  astraunaut,
+  gradient,
+  javaLogo,
+  pythonLogo,
+  notesLogo,
+  openEvent,
+  user,
+  userRegister,
+};
+export default assets;
